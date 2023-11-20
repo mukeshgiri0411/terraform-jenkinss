4dc21304a8995195fc2dfa14001389d936de1e40
mukeshgiri0411 <101009309+mukeshgiri0411@users.noreply.github.com>
2023-11-20 10:28:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,50 @@
 provider "aws" {
-    region = "eu-central-1"  
+  region     = "eu-central-1"
+  access_key = "AKIAQMFGQIMQ3P57JUOX"
+  secret_key = "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"
+
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
   tags = {
-      Name = "TF-Instance"
+    Name = "test_vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_security_group" "my_sg" {
+  name   = "HTTP"
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress  {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress  {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "my_instance" {
+  ami                    = "ami-04376654933b081a7"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  tags = {
+    "Name" = "MyEC2Instance"
+
   }
 }
